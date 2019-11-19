57217a5c42a90d5c25ecb0bce7a660281f792847
kademanec <kademanec@gmail.com>
2019-11-19 08:33:22 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'taskhomepage.dart';
+//import 'saleshomepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firestore Chart',
+      theme: ThemeData(
+        primaryColor: Color(0xff543B7A),
+      ),
+      home: TaskHomePage(),
+//      home:SalesHomePage(),
+    );
+  }
+}
